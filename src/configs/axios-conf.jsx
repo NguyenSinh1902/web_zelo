e7dac456feaaf97864aa578fbf39eaeeaabafdc3
NguyenSinh1902 <lenguyensinh1902@gmail.com>
2024-05-25 01:16:06 +0700

    https://server-zelo.onrender.com/

--- src/configs/axios-conf.jsx
+++ src/configs/axios-conf.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://server-zelo.onrender.com/',
+  baseURL: '18.141.86.142',
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
